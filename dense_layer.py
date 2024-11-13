[ADD] added base class init in subclasses [UPD] updated bias gradient computation [FIX] fixed error averaging to divide by num of data points (big X not small x)

dense_layer.py
@@ -3,7 +3,8 @@ import numpy as np
 
 class Dense(Layer):
     def __init__(self, input_size, output_size):
-        self.weights = np.random.randn(output_size, input_size)
+        super().__init__()
+        self.weights = np.random.randn(output_size, input_size) * 0.01
         self.bias = np.random.randn(output_size, 1)
 
     def forward(self, input):
@@ -12,10 +13,9 @@ class Dense(Layer):
     
     def backward(self, output_gradient, learning_rate):
         weights_gradient = np.dot(output_gradient, self.input.T)
+        bias_gradient = np.sum(output_gradient, axis=1, keepdims=True)
         self.weights -= learning_rate * weights_gradient
-
+        # self.bias -= learning_rate * bias_gradient
         self.bias -= learning_rate * np.sum(output_gradient, axis=1, keepdims=True)
-        
-        
         return np.dot(self.weights.T, output_gradient)
         